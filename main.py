5c175468475b40f8e54a3ed156daea4320d06a2c
Cata400 <cata0310@gmail.com>
2022-12-19 02:07:05 +0200

    added testing functionality for the tensorflow model, confusion matrix and ROC curve functions and docstrings for every function made so far

--- main.py
+++ main.py
@@ -8,22 +8,32 @@ if __name__ == '__main__':
     no_classes = len(fruit_list) 
     
     tensorflow = True
-    train = True
-    extract_data = False
+    train = False
+    extract_data = True
     pca = False
     no_components = 50 ** 2 * 3
     
     seed = 42
     batch_size = 512
     shuffle_buffer_size = 16 * batch_size
-    save_model_name = 'tf_pca50_3_v2.h5'
+    save_model_name = 'tf_pca50_2.h5'
     callbacks = [
                 TensorBoard(log_dir='../Logs/log_' + save_model_name.split('.')[0] + '_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")),
                 ModelCheckpoint(os.path.join('..', 'Models', save_model_name), monitor='val_loss', verbose=1, save_best_only=True),
     ]
     epochs = 150
+    
+    load_model_name = 'tf_nopca_1.h5'
 
     if tensorflow:
+        if pca:
+            train_tfrecord = 'train_pca' + str(int(np.sqrt(no_components // 3))) + '.tfrecord'
+            val_tfrecord = 'val_pca' + str(int(np.sqrt(no_components // 3))) + '.tfrecord'
+            test_tfrecord = 'test_pca' + str(int(np.sqrt(no_components // 3))) + '.tfrecord'
+        else:
+            train_tfrecord = 'train.tfrecord'
+            val_tfrecord = 'val.tfrecord'
+            test_tfrecord = 'test.tfrecord'
         if train:
         ### Get data
             if extract_data:
@@ -31,10 +41,11 @@ if __name__ == '__main__':
                     get_data_tf_pca(dataset_path, seed, no_classes, no_components)
                 else:
                     get_data_tf(dataset_path, seed, no_classes)
-                exit()  
+                exit()
                 
-            train_dataset = load_data_tf(os.path.join('..', 'Data', 'train.tfrecord'))
-            val_dataset = load_data_tf(os.path.join('..', 'Data', 'val.tfrecord'))
+            ### Load data
+            train_dataset = load_data_tf(os.path.join('..', 'Data', train_tfrecord))
+            val_dataset = load_data_tf(os.path.join('..', 'Data', val_tfrecord))
             
             train_dataset = train_dataset.cache()
             val_dataset = val_dataset.cache()
@@ -47,10 +58,39 @@ if __name__ == '__main__':
             train_dataset = train_dataset.map(fixup_shape)
             val_dataset = val_dataset.map(fixup_shape)
 
-
-            
             ### Initialize the model and train
             model = get_model_tf((100 * 100 * 3,), no_classes)
             model.fit(train_dataset, epochs=epochs, validation_data=val_dataset, callbacks=callbacks, verbose=1)
+            
+        else:
+            ### Load data
+            test_dataset = load_data_tf(os.path.join('..', 'Data', test_tfrecord))
+            
+            ### Get true labels
+            y_true = []
+            for example in test_dataset.take(-1):
+                y_true.append(example[1].numpy())
+            y_true = np.asanyarray(y_true)
+            y_true_labels = np.argmax(y_true, axis=1)
+            
+            ### Batch and shuffle the datasets
+            test_dataset = test_dataset.batch(batch_size)
+            
+            test_dataset = test_dataset.map(fixup_shape)
+            
+            ### Load model and predict
+            model = tf.keras.models.load_model(os.path.join('..', 'Models', load_model_name))
+            y_pred = model.predict(test_dataset, batch_size=batch_size, verbose=0)
+            y_pred_labels = np.argmax(y_pred, axis=1)
+            loss, acc = model.evaluate(test_dataset, verbose=0)
+            
+            print("Test accuracy: {:5.2f}%".format(100 * acc))
+            
+            ### Get confusion matrix
+            get_confusion_matrix(y_true_labels, y_pred_labels, fruit_list, print_acc=True)
+            
+            ### Get ROC AUC
+            get_roc_auc(y_true, y_pred, no_classes)
+
             
     print("Gata proiectul, 10!!")
--- utils_tf.py
+++ utils_tf.py
@@ -1,21 +1,53 @@
 import os
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 import tensorflow as tf
+
 from skimage import io
 from sklearn.decomposition import PCA, IncrementalPCA
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Model
 
 
 def read_image(path):
+    """
+    Read image from path.
+
+    Parameters
+    ----------
+    path : string
+        Path-like object.
+
+    Returns
+    -------
+    numpy.ndarray
+        Image to be processed.
+    """
     image = io.imread(path)
     return image
 
 
 def show_image(img, title, min=0, max=255, gray=False):
+    """
+    Plot image.
+
+    Parameters
+    ----------
+    img : 2D or 3D array-like
+        Image to be plotted.
+    title : string
+        Title of the plot.
+    min : int, optional
+        Minimum value for the plot, by default 0.
+    max : int, optional
+        Maximum value for the plot, by default 255.
+    gray : bool, optional
+        True if the image is grayscale, False otherwise, by default False.
+    """
     plt. figure()
     if gray:
         plt.imshow(img, cmap='gray', vmin=min, vmax=max)
@@ -28,6 +60,14 @@ def show_image(img, title, min=0, max=255, gray=False):
 
 
 def parse_dataset(path):
+    """
+    Parse dataset and print which images are not in the correct format.
+
+    Parameters
+    ----------
+    path : string
+        Path-like object.
+    """
     splits = [os.path.join(path, 'Training'), os.path.join(path, 'Test')]
 
     for folder in splits:
@@ -40,6 +80,19 @@ def parse_dataset(path):
 
 
 def _bytes_feature(value):
+    """
+    Returns a bytes_list from a string / byte.
+
+    Parameters
+    ----------
+    value : numpy.ndarray
+        Value to be converted.
+
+    Returns
+    -------
+    tensorflow.train.Feature
+        Feature to be written in the tfrecord file.
+    """
     if isinstance(value, type(tf.constant(0))):
         value = value.numpy()
     return tf.train.Feature(bytes_list=tf.train.BytesList(
@@ -48,8 +101,21 @@ def _bytes_feature(value):
 
 def serialize_example(img, label):
     """
-    Creates a tf.train.Example message ready to be written to a file.
+    Create a tf.train.Example message ready to be written to a file.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        Image to be serialized.
+    label : tensorflow.Tensor
+        Label to be serialized.
+
+    Returns
+    -------
+    tensorflow.train.Example
+        Example to be written in the tfrecord file.
     """
+    
     # Create a dictionary mapping the feature name to the tf.train.Example-compatible
     # data type.
     feature = {
@@ -63,6 +129,18 @@ def serialize_example(img, label):
             
                     
 def get_data_tf(path, seed, no_classes):
+    """
+    Write tfrecord files.
+
+    Parameters
+    ----------
+    path : string
+        Path-like object.
+    seed : int
+        Seed for the random generator.
+    no_classes : int
+        Number of classes.
+    """
     train_writer = tf.io.TFRecordWriter(os.path.join('..', 'Data', 'train.tfrecord'))
     test_writer = tf.io.TFRecordWriter(os.path.join('..', 'Data', 'test.tfrecord'))
     val_writer = tf.io.TFRecordWriter(os.path.join('..', 'Data', 'val.tfrecord'))
@@ -85,15 +163,29 @@ def get_data_tf(path, seed, no_classes):
                     y = tf.one_hot(i, no_classes)
                     
                     if folder == 'Training':
-                        train_writer.write(serialize_example(np.asanyarray(x, dtype=np.float32), np.asanyarray(y, dtype=np.float32)))
+                        train_writer.write(serialize_example(np.asanyarray(x, dtype=np.float32), y))
                     elif j in val_idx:
-                        val_writer.write(serialize_example(np.asanyarray(x, dtype=np.float32), np.asanyarray(y, dtype=np.float32)))
+                        val_writer.write(serialize_example(np.asanyarray(x, dtype=np.float32), y))
                     else:
-                        test_writer.write(serialize_example(np.asanyarray(x, dtype=np.float32), np.asanyarray(y, dtype=np.float32)))
+                        test_writer.write(serialize_example(np.asanyarray(x, dtype=np.float32), y))
 
 
 
 def get_data_tf_pca(path, seed, no_classes, no_components):
+    """
+    Write tfrecord files with PCA.
+
+    Parameters
+    ----------
+    path : string
+        Path-like object.
+    seed : int
+        Seed for the random generator.
+    no_classes : int
+        Number of classes.
+    no_components : int
+        Number of components for PCA.
+    """
     train_writer = tf.io.TFRecordWriter(os.path.join('..', 'Data', 'train_pca' + str(int(np.sqrt(no_components // 3))) + '.tfrecord'))
     test_writer = tf.io.TFRecordWriter(os.path.join('..', 'Data', 'test_pca' + str(int(np.sqrt(no_components // 3))) + '.tfrecord'))
     val_writer = tf.io.TFRecordWriter(os.path.join('..', 'Data', 'val_pca' + str(int(np.sqrt(no_components // 3))) + '.tfrecord'))
@@ -155,6 +247,21 @@ def get_data_tf_pca(path, seed, no_classes, no_components):
                 
 
 def _parse_function(example_proto):
+    """
+    Parse a single example.
+
+    Parameters
+    ----------
+    example_proto : tensorflow.string
+        Example proto.
+
+    Returns
+    -------
+    decoded_img : tensorflow.Tensor
+        Decoded image.
+    decoded_label : tensorflow.Tensor
+        Decoded label.
+    """
     feature_description = {
         'img': tf.io.FixedLenFeature([], tf.string, default_value=''),
         'label': tf.io.FixedLenFeature([], tf.string, default_value=''),
@@ -164,11 +271,23 @@ def _parse_function(example_proto):
     decoded_img = tf.io.parse_tensor(element['img'], 'float32')
     decoded_label = tf.io.parse_tensor(element['label'], 'float32')
 
-    # Parse the input `tf.train.Example` proto using the dictionary above.
     return decoded_img, decoded_label
 
 
 def load_data_tf(path):
+    """
+    Load data from tfrecord files.
+
+    Parameters
+    ----------
+    path : string
+        Path-like object.
+
+    Returns
+    -------
+    tensorflow.data.Dataset
+        Dataset with images and labels.
+    """
     raw_dataset = tf.data.TFRecordDataset(path)
     parsed_dataset = raw_dataset.map(_parse_function)
     
@@ -176,12 +295,27 @@ def load_data_tf(path):
 
 
 def get_model_tf(input_shape, no_classes):
+    """
+    Get simple MLP model with 6 hidden layers.
+
+    Parameters
+    ----------
+    input_shape : list or tuple of int
+        Shape of input.
+    no_classes : int
+        Number of classes, used as number of neurons in the output layer of the Neural Network.
+
+    Returns
+    -------
+    tensorflow.keras.Model
+        Model of MLP with 6 hidden layers.
+    """
     model_input = Input(shape=input_shape)
-    h1 = Dense(2048, activation='relu')(model_input)
-    h2 = Dense(1024, activation='relu')(h1)
-    h3 = Dense(1024, activation='relu')(h2)
+    h1 = Dense(1024, activation='relu')(model_input)
+    h2 = Dense(512, activation='relu')(h1)
+    h3 = Dense(512, activation='relu')(h2)
     h4 = Dense(512, activation='relu')(h3)
-    h5 = Dense(512, activation='relu')(h4)
+    h5 = Dense(256, activation='relu')(h4)
     h6 = Dense(256, activation='relu')(h5)
     output = Dense(no_classes, activation='softmax')(h6)
     
@@ -194,9 +328,88 @@ def get_model_tf(input_shape, no_classes):
 
 
 def fixup_shape(images, labels):
+    """
+    Fixup shape of images and labels.
+
+    Parameters
+    ----------
+    images : tensorflow.Tensor
+        Image to be fed into the model.
+    labels : tensorflow.Tensor
+        Label used for the output of the model.
+
+    Returns
+    -------
+    images: tensorflow.Tensor
+        Image with a fixed shape.
+    labels: tensorflow.Tensor
+        Label with a fixed shape.
+    """
     images.set_shape([None, 100 * 100 * 3])
     labels.set_shape([None, 131])
     return images, labels
 
+
+def get_confusion_matrix(y_true, y_pred, fruit_list, plot_cm=True, scale=10, print_acc=False):
+    """
+    Get confusion matrix.
+
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples,)
+        True labels.
+    y_pred : array-like of shape (n_samples,)
+        Predicted labels.
+    fruit_list : list or tuple of shape (n_samples,) 
+        List of labels.
+    plot_cm : bool, optional
+        True if the confusion matrix is to be plotted, False otherwise, by default True.
+    scale : int, optional
+        Scale of the plotted confusion matrix for better reading, by default 10.
+    print_acc : bool, optional
+        True to print the per-class accuracy for every class, False otherwise, by default False.
+    """
+    cm = confusion_matrix(y_true, y_pred, normalize='true')
+    
+    if plot_cm:
+        cm_cut = cm[:scale, :scale]
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm_cut, display_labels=fruit_list[:scale])
+        disp.plot()
+        plt.show()
+    
+    if print_acc:
+        for i, fruit in enumerate(cm):
+            print(f'{fruit_list[i]}: {np.round(100 * fruit[i],2 )}% accuracy' )
+    
+    
+def get_roc_auc(y_true, y_pred, no_classes):
+    """
+    Get ROC AUC.
+
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples, n_classes), should be in categorical format
+        True labels.
+    y_pred : array-like of shape (n_samples, n_classes), should be in categorical format
+        Predicted labels.
+    no_classes : int
+        Number of classes.
+    """
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    colors = sorted(mcolors.CSS4_COLORS.keys())
+    
+    plt.figure()
+    for i in range(no_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        plt.plot(fpr[i], tpr[i], color=colors[i])
+    plt.plot([0, 1], [0, 1], color=colors[no_classes], linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.show()
+        
+    
+    
 
 
